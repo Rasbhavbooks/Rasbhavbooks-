015d27aeee01cdf7cd3f699439eecf367a3e8733
Rasbhavbooks <rasbhavbooks@gmail.com>
2026-09-13 23:58:01 +0530

    Add admin route and authentication check

--- app/__init__.py
+++ app/__init__.py
@@ -18,9 +18,11 @@ def create_app():
     from app.routes.category import category_bp
     from app.routes.author import author_bp
     from app.routes.search import search_bp
+    from app.routes.admin import admin_bp
     app.register_blueprint(category_bp)
     app.register_blueprint(author_bp)
     app.register_blueprint(search_bp)
+    app.register_blueprint(admin_bp)
 
     @app.route("/")
     def home():
