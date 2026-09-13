e43c49870f4d634ee4cfe926025a80f8862c0a02
Rasbhavbooks <rasbhavbooks@gmail.com>
2026-09-13 22:22:24 +0530

    Add favorite model

---
+++ app/models/favorite.py
@@ -0,0 +1,25 @@
+from app import db
+from datetime import datetime
+
+
+class Favorite(db.Model):
+    __tablename__ = "favorites"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"),
+        nullable=False
+    )
+
+    book_id = db.Column(
+        db.Integer,
+        db.ForeignKey("books.id"),
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
