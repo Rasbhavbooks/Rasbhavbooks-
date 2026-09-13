1494b78411d8310273e328589943d71699bd32f6
Rasbhavbooks <rasbhavbooks@gmail.com>
2026-09-14 00:07:50 +0530

    Register admin_chapters blueprint in app

--- app/__init__.py
+++ app/__init__.py
@@ -21,12 +21,15 @@ def create_app():
     from app.routes.admin import admin_bp
     from app.routes.user import user_bp
     from app.routes.admin_books import admin_books_bp
+    from app.routes.admin_chapters import admin_chapters_bp 
+    
     app.register_blueprint(category_bp)
     app.register_blueprint(author_bp)
     app.register_blueprint(search_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(admin_books_bp)
+    app.register_blueprint(admin_chapters_bp)
 
     @app.route("/")
     def home():
